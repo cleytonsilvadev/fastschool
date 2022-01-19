a9dc0ae1828c57d90f3f6f1b675b64fc5ccf922c
Cleyton Rodrigues <88549697+cleytonsilvadev@users.noreply.github.com>
2022-01-18 22:48:08 -0300

    Add files via upload

---
+++ utilizando o print.py
@@ -0,0 +1,3 @@
+print('hello world')
+print('segundo print')
+print('teste')
---
+++ variaveis tipos operadores.py
@@ -0,0 +1,36 @@
+
+#variaveis
+nome = 'Cleyton Rodrigues'
+idade = 25
+altura = 1.70
+
+#sainda em tela 'print'
+print(nome)
+
+# 'type' serve para identificar o tipo da variavel
+print(type(nome))
+print(type(idade))
+print(type(altura))
+
+# concatenando ' , ' as variaveis
+print(nome, 'tem', idade, 'anos de idade e de altura tem', altura)
+
+#entrada de dados
+
+rua = input('onde voce mora?')
+
+#operadores
+numero1 = 15
+numero2 = 10
+
+soma = (numero1 + numero2)
+sub = (numero1 - numero2)
+div = (numero1 / numero2)
+mult = (numero1 * numero2)
+
+
+
+print(soma,'\n',sub,'\n',div,'\n',mult,'\n')
+
+
+
